d0a467175ec012506ad09cd2195cc750da0ac647
Blaidd_ddrwg613 <danielleger2011@gmail.com>
2024-07-28 17:55:37 -0400

    - Added Haste block

---
+++ src/main/java/io/github/ddrwg613/stuffnthings/block/HasteBlock.java
@@ -0,0 +1,25 @@
+package io.github.ddrwg613.stuffnthings.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class HasteBlock extends Block {
+
+    public HasteBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        LivingEntity entity = pEntity instanceof LivingEntity ? (LivingEntity)pEntity : null;
+        if (entity != null) {
+            entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,60,2));
+        }
+    }
+}
--- src/main/java/io/github/ddrwg613/stuffnthings/block/ModBlocks.java
+++ src/main/java/io/github/ddrwg613/stuffnthings/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SupportType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -44,6 +45,10 @@ public class ModBlocks {
             ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_EMERALD_ORE)
                     .requiresCorrectToolForDrops()
             ));
+    public static final RegistryObject<HasteBlock> HASTE_BLOCK = registerBlock("haste_block",
+            ()-> new HasteBlock(BlockBehaviour.Properties.of()
+                    .strength(2.0f,5.0f)
+                    .instrument(NoteBlockInstrument.BASS)));
 
     public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         // Register the block
